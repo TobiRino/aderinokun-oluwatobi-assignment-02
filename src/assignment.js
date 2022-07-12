9a2d75595eb6b8435625669277944ae00be97072
Oluwatobi Aderinokun <91747492+TobiRino@users.noreply.github.com>
2022-07-12 10:54:07 +0000

    first real commit

--- src/assignment.js
+++ src/assignment.js
@@ -13,11 +13,14 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function sumOfNumbers(arrayOfNumbers){
+let sum = 0;
+for(let i = 0; i < arrayOfNumbers.length; i++){
+  sum += arrayOfNumbers[i];
+}
+return sum;
 }
 assignment.sumOfNumbers = sumOfNumbers;
-
 /**
  * Challenge - 2
  * 
@@ -28,7 +31,14 @@ assignment.sumOfNumbers = sumOfNumbers;
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let newArray= [];
+    for(let i=0; i<arrayOfNumbers.length; i++){
+        let even= arrayOfNumbers[i];
+    if(even % 2 === 0 ){
+        newArray.push(even)
+    }
+    }
+    return newArray.length;
 }
 assignment.countEvenNumbers = countEvenNumbers;
 
